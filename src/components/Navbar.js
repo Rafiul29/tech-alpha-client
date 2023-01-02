ac2838e79e5a9961d820feca0e9ca2c5de45fc91
Rafiul29 <rafiulislam4472@gmail.com>
2023-01-02 18:41:54 +0600

    d

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { BsCart3 } from 'react-icons/bs'
+
+
 const Navbar = () => {
   return (
     <div className='navbar-bg  bg-violet-500 text-violet-50 h-20  flex justify-center items-center'>
@@ -15,7 +17,7 @@ const Navbar = () => {
           <Link to="/cart">
             <span className='cart-icons relative'>
               <BsCart3 />
-              <span className='cart-counter absolute -top-0 -right-7 text-x5 bg-orange-600 h-5 w-5 rounded-full flex items-center justify-center font-me'> 10</span>
+              <span className='cart-counter absolute -top-3 -right-3 text-x5 bg-orange-600 h-5 w-5 rounded-full flex items-center justify-center font-me'> 10</span>
             </span>
           </Link>
         </div>
