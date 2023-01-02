23eee787b8a1ce681cfc707c5f8f90973dad2676
Rafiul29 <rafiulislam4472@gmail.com>
2023-01-02 17:38:01 +0600

    Navber added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import NotFound from './pages/NotFound'
+import Cart from './pages/Cart'
+
+
 
 const App = () => {
   return (
-    <div><h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1></div>
+   <>
+    <div className='app min-h-screen bg-gray-50 text-gray-700'>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/products' element={<Products/>}/>
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='*' element={<NotFound/>}/>
+        </Routes>
+    </div>
+    <Footer/>
+   </>
   )
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { BsCart3 } from 'react-icons/bs'
+const Navbar = () => {
+  return (
+    <div className='navbar-bg  bg-violet-500 text-violet-50 h-20  flex justify-center items-center'>
+      <div className='navbar container  mx-auto flex justify-between items-center'>
+        <div className='left'>
+          <span className='text-xl font-semibold '>tech
+          <span className='text-orange-500'>Alpha</span></span>
+      </div>
+        <div className='right flex items-center gap-5'>
+          <Link to="/" className='nav-link'>Home</Link>
+          <Link to="/products" className='nav-link  '>Products</Link>
+          <Link to="/cart">
+            <span className='cart-icons relative'>
+              <BsCart3 />
+              <span className='cart-counter absolute -top-0 -right-7 text-x5 bg-orange-600 h-5 w-5 rounded-full flex items-center justify-center font-me'> 10</span>
+            </span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
