1a1d8de4e6ba53a17ba05e2269f1030adf24e98d
Rafiul29 <rafiulislam4472@gmail.com>
2023-01-09 01:49:16 +0600

    redirects added

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className="container mx-auto py-8 flex flex-col mt-10 items-center gap-5">
+    <p className="text-2xl lg:text-4xl text-center font-semibold text-orange-400 ">
+       Page not found!
+     </p>
+     <Link
+       to="/"
+       className="text-violet-50 text-2xl bg-violet-400 px-5 py-3 rounded-full"
+     >
+       Go home
+     </Link>
+
+   </div>
   )
 }
 
